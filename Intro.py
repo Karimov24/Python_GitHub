dda5939e2d00636c4655600c93ea7fa78f37cfb9
Rakhmonjon Karimov <karimovrr23@gmail.com>
2024-11-08 11:16:35 -0500

    Hello!

---
+++ Intro.py
@@ -0,0 +1,4 @@
+#Intro to repository
+
+print("""Hello this is my Python repository folder. I will post codes here, from begginer level all up to intermidiate.
+      hopefully being consistent with contributions per-day.""")
